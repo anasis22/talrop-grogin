352fd404584733d08cde7f3a1a6420caa9bc78e6
anas ismail <msanasismail@gmail.com>
2024-07-02 18:45:28 +0530

    datas updated

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 const navBtns = document.querySelectorAll(".nav-links");
 
 navBtns.forEach((item) => {
-    item.addEventListener('click',event => {
+    item.addEventListener('click', event => {
         navBtns.forEach((item) => {
-           item.classList.remove('active')
+            item.classList.remove('active')
         })
         item.classList.add('active')
     })
 })
+
 
 // -------- CheckBox checked to color change --------
 
@@ -17,22 +18,50 @@ navBtns.forEach((item) => {
 const checkBox = document.querySelectorAll('.productCategoriesCheckbox')
 
 checkBox.forEach((item) => {
-    item.addEventListener('change', function() {
+    item.addEventListener('change', function () {
         const element = this.nextElementSibling;
-         if(this.checked){
+        if (this.checked) {
             element.classList.add('checked-text')
-         }else{
+        } else {
             element.classList.remove('checked-text')
-         }
+        }
     })
 })
 
 // json testing
 
-fetch('./fruits.json').then((res) => {
-    res.json()
-    .then((data) => {
-        console.log(data)
-    })
-})
+// fetch('./data.json').then((res) => {
+//     res.json()
+//     .then((data) => {
+//         console.log(data)
+//     })
+// })
 
+document.addEventListener('DOMContentLoaded', function () {
+    const resultContainer = document.querySelector('.results');
+    const productCard = document.createElement('ul');
+    productCard.className = "productCard"
+
+    async function mapping() {
+        try {
+            const res = await fetch('./data.json');
+            const data = await res.json();
+
+            data.forEach((el) => {
+                const productCardLists = document.createElement('li')
+
+                productCardLists.className = "productCardLists"
+
+                productCardLists.innerHTML = el.name;
+
+                productCard.appendChild(productCardLists)
+                resultContainer.appendChild(productCard);
+            });
+
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    mapping();
+});
