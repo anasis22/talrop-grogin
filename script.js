728d9674b28b4a58518d9dd210bbfd898e133673
anas ismail <msanasismail@gmail.com>
2024-07-05 09:44:22 +0530

    mapping

--- script.js
+++ script.js
@@ -13,32 +13,38 @@ navBtns.forEach((item) => {
 
 // -------- CheckBox checked to color change --------
 
-const checkBox = document.querySelectorAll('.productCategoriesCheckbox')
+  const checkBox = document.querySelectorAll('.productCategoriesCheckbox');
 
-checkBox.forEach((item) => {
-    item.addEventListener('change', function () {
-        const element = this.nextElementSibling;
-        if (this.checked) {
-            element.classList.add('checked-text')
-        } else {
-            element.classList.remove('checked-text')
-        }
-    })
-})
+  checkBox.forEach((item) => {
+      item.addEventListener('change', function () {
+          const element = this.nextElementSibling;
+          if (this.checked) {
+              element.classList.add('checked-text')
+          } else if (!this.checked) {
+              element.classList.remove('checked-text')
+              
+          }
+      })
+  })
 
-// --------
+// ----------- Product Lists -----------
 
 document.addEventListener('DOMContentLoaded', function () {
     const resultContainer = document.querySelector('.results');
     const productCard = document.createElement('ul');
     productCard.className = "productCard"
+    const otherProductsCard = document.createElement('ul')
+    otherProductsCard.className = 'otherProductsCard';
+    let allProducts = []
 
     async function mapping() {
         try {
             const res = await fetch('./data.json');
             const data = await res.json();
+            allProducts = data
 
-            data.forEach((el) => {
+
+            allProducts.forEach((el) => {
                 const productCardLists = document.createElement('li')
 
                 // topLi
@@ -48,13 +54,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 const rightImgTag = document.createElement('img');
                 leftImgTag.className = 'leftImgTag';
                 rightImgTag.className = 'rightImgTag';
-                leftImgTag.src = './assets/offer-badge.png'
+                leftImgTag.src = el.offerBadge
                 rightImgTag.src = './assets/heart.png'
                 topLi.appendChild(leftImgTag);
                 topLi.appendChild(rightImgTag);
 
                 // heart
-                rightImgTag.addEventListener('click',() => {
+                rightImgTag.addEventListener('click', () => {
                     if (rightImgTag.src.includes('heart.png')) {
                         rightImgTag.src = './assets/heartRed.png';
                     } else if (rightImgTag.src.includes('heartRed.png')) {
@@ -65,19 +71,61 @@ document.addEventListener('DOMContentLoaded', function () {
                 // midLi
                 const midLi = document.createElement("li");
                 midLi.className = "midLi"
-                const midImg = document.createElement('img');
+                const midImg = document.createElement("img");
                 midImg.className = "midImg";
                 midImg.src = el.img
                 midLi.appendChild(midImg)
 
-
                 // bottomLi
                 const bottomLi = document.createElement("li");
-                bottomLi.className = "bottom"
-                // const ratingStarImg = document.createElement("img")
-                // ratingStarImg.className = 'ratingStarImg'
-                // ratingStarImg.src = el.ratingStarImg
-                // bottomLi.appendChild('ratingStarImg')
+                bottomLi.className = "bottomLi"
+
+                const productName = document.createElement("p");
+                productName.className = 'productName'
+                productName.innerHTML = el.name
+                bottomLi.appendChild(productName)
+
+                const ratingLi = document.createElement("li");
+                ratingLi.className = "ratingLi"
+                bottomLi.appendChild(ratingLi);
+
+                const ratingStarImg = document.createElement("img")
+                ratingStarImg.className = 'ratingStarImg'
+                ratingStarImg.src = el.ratingStarImg
+                ratingLi.appendChild(ratingStarImg)
+
+                const itemCounts = document.createElement("p")
+                itemCounts.className = 'itemCounts'
+                itemCounts.innerHTML = el.itemCounts
+                ratingLi.appendChild(itemCounts)
+
+                const priceLi = document.createElement("li");
+                priceLi.className = "priceLi"
+                bottomLi.appendChild(priceLi);
+
+                const price = document.createElement("p")
+                price.className = 'price'
+                price.innerHTML = el.price
+                priceLi.appendChild(price)
+
+                const priceWrong = document.createElement("p")
+                priceWrong.className = 'priceWrong'
+                priceWrong.innerHTML = el.wrongPrice
+                priceLi.appendChild(priceWrong)
+
+                const inStockLi = document.createElement("li");
+                inStockLi.className = "inStockLi"
+                bottomLi.appendChild(inStockLi);
+
+                const inStockImg = document.createElement("img")
+                inStockImg.className = 'inStockImg'
+                inStockImg.src = el.inStockImg
+                inStockLi.appendChild(inStockImg)
+
+                const inStockText = document.createElement("p")
+                inStockText.className = 'inStockText'
+                inStockText.innerHTML = 'IN STOCK'
+                inStockLi.appendChild(inStockText)
 
                 productCardLists.className = "productCardLists"
 
@@ -86,12 +134,55 @@ document.addEventListener('DOMContentLoaded', function () {
                 productCardLists.appendChild(bottomLi)
                 productCard.appendChild(productCardLists)
                 resultContainer.appendChild(productCard);
+
             });
 
         } catch (error) {
             console.error('Error fetching data:', error);
         }
+
     }
 
-    mapping();
+    mapping()
+
+    // const filteredProducts = () => {
+    //     const checkedCategories = Array.from(checkBox)
+    //     const filteredCheckedCategories = checkedCategories.filter(checkbox => checkbox.checked)
+    //     console.log(filteredCheckedCategories)
+    //     // console.log(checkedCategories)
+    // }
+
+    // filteredProducts()
+
+    // async function filtering() {
+    //     try {
+    //         const res = await fetch('./data.json');
+    //         const data = await res.json();
+
+    //         data.filter((el) => {
+    //             if (el.name === "Other Products") {
+
+    //                 const otherProductsCardLi = document.createElement('li');
+    //                 otherProductsCardLi.className = 'otherProductsCardLi';
+
+    //                 const otherProductText = document.createElement('p');
+    //                 otherProductText.className = 'otherProductText';
+    //                 otherProductText.innerHTML = 'Other Products';
+    //                 otherProductsCardLi.appendChild(otherProductText);
+
+    //                 console.log(otherProductsCard)
+    //                 otherProductsCard.appendChild(otherProductsCardLi);
+    //                 resultContainer.appendChild(otherProductsCard)
+    //             };
+
+    //         })
+    //     } catch (error) {
+    //         console.error('Error fetching data:', error);
+    //     }
+
+    // }
+
+    // filtering()
+
+
 });
